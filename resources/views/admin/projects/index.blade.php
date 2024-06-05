de7e26a3cb9167f1e423b89a0520f210eb77cb1a
RoccoCerro <rcerro02@gmail.com>
2024-06-05 22:09:30 +0200

    Add: style with bootstrap on view index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,36 +3,40 @@
 @section('content')
 
 <div class="container">
-  <div>
-    <a class="btn btn-dark" href="{{ route('admin.projects.create')}}">New Project</a>
+  <div class="row justify-content-end">
+    <div class="col-auto p-3">
+      <a class="btn btn-dark" href="{{ route('admin.projects.create')}}">New Project</a>
+    </div>
   </div>
 
-  <table class="table">
+  <table class="table table-dark table-striped">
     <thead>
       <tr>
         <th>Title</th>
         <th>Slug</th>
         <th>Content</th>
+        <th>Edit</th>
+        <th>Delete</th>
       </tr>
     </thead>
     <tbody>
       @foreach ($projects as $project)
       <tr>
         <td>
-          <a href="{{ route('admin.projects.show', $project) }}">
+          <a class="text-white text-decoration-none" href="{{ route('admin.projects.show', $project) }}">
             {{ $project->title }}
           </a>
         </td>
         <td>{{$project->slug}}</td>
         <td>{{$project->content}}</td>
-        <td><a href="{{ route('admin.projects.edit', $project) }}">modifica</a></td>
+        <td><a class="btn btn-text-decoration-none text-white btn-secondary" href="{{ route('admin.projects.edit', $project) }}">modifica</a></td>
         <td>
           <form action="{{ route('admin.projects.destroy',$project) }}" method="POST">
                   
           @csrf
           @method('DELETE')
 
-          <button class="btn btn-link link-danger">Elimina</button>
+          <button class="btn btn-danger link-danger text-white">Elimina</button>
         
           </form>
         </td>
