ccfc1d44a2d84219a974f44beac975ae58771259
RoccoCerro <rcerro02@gmail.com>
2024-06-06 12:37:24 +0200

    Add: views create

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -24,7 +24,7 @@ class TypeController extends Controller
      */
     public function create()
     {
-        //
+        return view("admin.types.create");
     }
 
     /**
--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
   <div class="container p-5">
+    <div class="row justify-content-between">
+      <div class="col-auto">
+        <h1>Tipo</h1>
+      </div>
+      <div class="col-auto">
+        <a class="btn btn-dark" href="{{ route('admin.types.create') }}">Nuovo tipo</a>
+      </div>
+    </div>
+  </div>
+  <div class="container">
     <div class="row">
-      <h1>Tipo</h1>
       @foreach ($types as $type)
         <div class="col">
           <div class="card">
